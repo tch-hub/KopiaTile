42ddef8c7e2026e342fa4f8d4330d796ac055220
tch-hub <tch27work@gmail.com>
2026-03-10 10:45:46 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
+	],
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib")
+		}
+	}
+});
